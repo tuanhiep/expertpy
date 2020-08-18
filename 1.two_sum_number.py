80baa543ab78dd50ec3c4e54904be49c82308697
bigname <tuanhiep.tran88@gmail.com>
2020-08-18 16:44:30 -0600

    solve problem of two number sum solution 4

--- 1.two_sum_number.py
+++ 1.two_sum_number.py
@@ -28,3 +28,19 @@ def twoNumberSum3(array, targetSum):
         else:
             nums[num] = True
     return []
+
+
+def twoNumberSum4(array, targetSum):
+    # Write your code here.
+    array.sort()
+    left = 0
+    right = len(array) - 1
+    while left < right:
+        currentSum = array[left] + array[right]
+        if currentSum == targetSum:
+            return [array[left], array[right]]
+        elif currentSum < targetSum:
+            left += 1
+        elif currentSum > targetSum:
+            right -= 1;
+    return []
