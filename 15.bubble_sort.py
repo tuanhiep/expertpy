56d1721d83696f5b2a0c63e21082dba366e91091
bigname <tuanhiep.tran88@gmail.com>
2021-03-06 21:14:59 -0700

    bubble sort solution

---
+++ 15.bubble_sort.py
@@ -0,0 +1,10 @@
+def bubbleSort(array):
+    # Write your code here.
+    for i in range(len(array)-1):
+		for j in range(len(array)-i-1):
+			if array[j]> array[j+1]:
+				tmp= array[j]
+				array[j]=array[j+1]
+				array[j+1]=tmp
+	return array
+
