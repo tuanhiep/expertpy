9be06fde8c38e0627cb0ed554754c3814fd5857d
bigname <tuanhiep.tran88@gmail.com>
2021-03-07 11:00:50 -0700

    insertion sort solution

---
+++ 20.insertion_sort.py
@@ -0,0 +1,9 @@
+def insertionSort(array):
+    # Write your code here.
+    for i in range(1, len(array)):
+        for j in reversed(range(i)):
+            if array[j + 1] < array[j]:
+                tmp = array[j + 1]
+                array[j + 1] = array[j]
+                array[j] = tmp
+    return array
