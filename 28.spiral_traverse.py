374744a33176da8d8304921fa45e391faceedca0
bigname <tuanhiep.tran88@gmail.com>
2021-03-08 10:02:35 -0700

    spiral traverse solution

--- 28.spiral_traverse.py
+++ 28.spiral_traverse.py
@@ -37,3 +37,32 @@ def spiralTraverse(array):
     return result
 
 
+def spiralTraverse(array):
+    # Write your code here.
+    result = []
+    start_row = 0
+    end_row = len(array) - 1
+    start_column = 0
+    end_column = len(array[0]) - 1
+
+    while start_row <= end_row and start_column <= end_column:
+
+        for c in range(start_column, end_column + 1):
+            result.append(array[start_row][c])
+        for r in range(start_row + 1, end_row + 1):
+            result.append(array[r][end_column])
+        for c in reversed(range(start_column, end_column)):
+            if start_row == end_row:
+                break
+            result.append(array[end_row][c])
+        for r in reversed(range(start_row + 1, end_row)):
+            if start_column == end_column:
+                break
+            result.append(array[r][start_column])
+
+        start_row += 1
+        end_row -= 1
+        start_column += 1
+        end_column -= 1
+
+    return result
