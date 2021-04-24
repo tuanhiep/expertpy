4293be6378f8ae3237bde2e0fc46dbb6bdf2fc35
bigname <tuanhiep.tran88@gmail.com>
2021-04-24 16:28:54 -0600

    find successor of a node by in-order traversal in BST Tree solution

--- 43.find_successor.py
+++ 43.find_successor.py
@@ -25,3 +25,38 @@ def traverseInOrder(tree, node, array):
         return
     array.append(tree)
     traverseInOrder(tree.right, node, array)
+
+
+# Solution 2
+
+# This is an input class. Do not edit.
+class BinaryTree:
+    def __init__(self, value, left=None, right=None, parent=None):
+        self.value = value
+        self.left = left
+        self.right = right
+        self.parent = parent
+
+
+def findSuccessor(tree, node):
+    # Write your code here.
+    if node.right is not None:
+        return getLeftmostChild(node.right)
+
+    return getRightmostParent(node)
+
+
+def getLeftmostChild(node):
+    currentNode = node
+    while currentNode.left is not None:
+        currentNode = currentNode.left
+
+    return currentNode
+
+
+def getRightmostParent(node):
+    currentNode = node
+    while currentNode.parent is not None and currentNode.parent.right == currentNode:
+        currentNode = currentNode.parent
+
+    return currentNode.parent
