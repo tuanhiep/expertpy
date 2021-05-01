e3af8486ddceaf7f3d58745ac831d317907d6ef4
bigname <tuanhiep.tran88@gmail.com>
2021-05-01 09:57:28 -0600

    remove kth node from end of a singly linked list solution

---
+++ 61.remove_kth_node_from_end.py
@@ -0,0 +1,24 @@
+# O(n) time | O(1) space
+# This is an input class. Do not edit.
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+def removeKthNodeFromEnd(head, k):
+    # Write your code here.
+    counter = 1
+    first = head
+    second = head
+    while counter <= k:
+        second = second.next
+        counter += 1
+    if second is None:
+        head.value = head.next.value
+        head.next = head.next.next
+        return
+    while second.next is not None:
+        second = second.next
+        first = first.next
+    first.next = first.next.next
