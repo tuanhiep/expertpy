c4fa3d7a416b85e2c3b98f2147ec33f4b421fc2d
bigname <tuanhiep.tran88@gmail.com>
2021-05-08 11:33:02 -0600

    Reverse Words In String Solution

---
+++ 76.reverse_words_in_string.py
@@ -0,0 +1,23 @@
+# O(n) time | O(n) space - where n is the length of the string
+def reverseWordsInString(string):
+    words = []
+    startOfWord = 0
+    for idx in range(len(string)):
+        character = string[idx]
+        if character == " ":
+            words.append(string[startOfWord:idx])
+            startOfWord = idx
+        elif string[startOfWord] == " ":
+            words.append(" ")
+            startOfWord = idx
+    words.append(string[startOfWord:])
+    reverseList(words)
+    return "".join(words)
+
+
+def reverseList(list):
+    start, end = 0, len(list) - 1
+    while start < end:
+        list[start], list[end] = list[end], list[start],
+        start += 1
+        end -= 1
