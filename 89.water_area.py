24939306e5da358c9e40bb23516fd0f4c2cbc1bc
bigname <tuanhiep.tran88@gmail.com>
2021-05-20 11:47:41 -0600

    Water Area Solution

---
+++ 89.water_area.py
@@ -0,0 +1,45 @@
+# Solution 1
+def waterArea(heights):
+    # O(n) time | O(n) space - where n is the length of input array
+    maxes = [0 for x in heights]
+    leftMax = 0
+    for i in range(len(heights)):
+        height = heights[i]
+        maxes[i] = leftMax
+        leftMax = max(leftMax, height)
+    rightMax = 0
+    for i in reversed(range(len(heights))):
+        height = heights[i]
+        minHeight = min(rightMax, maxes[i])
+        if height < minHeight:
+            maxes[i] = minHeight - height
+        else:
+            maxes[i] = 0
+        rightMax = max(rightMax, height)
+    return sum(maxes)
+
+
+# Solution 2
+
+def waterArea(heights):
+    # O(n) time | O(1) space - where n is the length of the input array
+    if len(heights) == 0:
+        return 0
+
+    leftIdx = 0
+    rightIdx = len(heights) - 1
+    leftMax = heights[leftIdx]
+    rightMax = heights[rightIdx]
+    surfaceArea = 0
+
+    while leftIdx < rightIdx:
+        if heights[leftIdx] < heights[rightIdx]:
+            leftIdx += 1
+            leftMax = max(leftMax, heights[leftIdx])
+            surfaceArea += leftMax - heights[leftIdx]
+        else:
+            rightIdx -= 1
+            rightMax = max(rightMax, heights[rightIdx])
+            surfaceArea += rightMax - heights[rightIdx]
+
+    return surfaceArea
