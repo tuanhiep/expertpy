8122c5a0d9c08ac789330e7ac702a8932d27d1a1
bigname <tuanhiep.tran88@gmail.com>
2021-06-01 12:09:40 -0600

    Reverse Linked List Solution

---
+++ 103.reverse_linked_list.py
@@ -0,0 +1,17 @@
+# This is an input class. Do not edit.
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+def reverseLinkedList(head):
+    # O(n) time | O(1) space - where n is the number of nodes in the
+    # Linked List
+    previousNode, currentNode = None, head
+    while currentNode is not None:
+        nextNode = currentNode.next
+        currentNode.next = previousNode
+        previousNode = currentNode
+        currentNode = nextNode
+    return previousNode
