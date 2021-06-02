fbfc04beacbbe5d3d5790dcf1a1645167ec8d2e7
bigname <tuanhiep.tran88@gmail.com>
2021-06-01 21:02:04 -0600

    Search For Range Solution 1

---
+++ 112.search_for_range.py
@@ -0,0 +1,28 @@
+# Solution 1
+def searchForRange(array, target):
+    # O(log(n)) time | O(log(n)) space
+    finalRange = [-1, -1]
+    alteredBinarySearch(array, target, 0, len(array) - 1, finalRange, True)
+    alteredBinarySearch(array, target, 0, len(array) - 1, finalRange, False)
+    return finalRange
+
+
+def alteredBinarySearch(array, target, left, right, finalRange, goLeft):
+    if left > right:
+        return
+    mid = (left + right) // 2
+    if array[mid] < target:
+        alteredBinarySearch(array, target, mid + 1, right, finalRange, goLeft)
+    elif array[mid] > target:
+        alteredBinarySearch(array, target, left, mid - 1, finalRange, goLeft)
+    else:
+        if goLeft:
+            if mid == 0 or array[mid - 1] != target:
+                finalRange[0] = mid
+            else:
+                alteredBinarySearch(array, target, left, mid - 1, finalRange, goLeft)
+        else:
+            if mid == len(array) - 1 or array[mid + 1] != target:
+                finalRange[1] = mid
+            else:
+                alteredBinarySearch(array, target, mid + 1, right, finalRange, goLeft)
