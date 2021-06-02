06a8b0a7d73cfd436220a81af359bb22c45c7e08
bigname <tuanhiep.tran88@gmail.com>
2021-06-02 08:08:31 -0600

    Index Equals Value Solution 2

--- 114.index_equals_value.py
+++ 114.index_equals_value.py
@@ -6,3 +6,28 @@ def indexEqualsValue(array):
         if index == value:
             return index
     return -1
+
+
+# Solution 2
+
+def indexEqualsValue(array):
+    # O(log(n)) time | O(log(n)) space - where n is the legth of
+    # the input array
+    return indexEqualsValueHelper(array, 0, len(array) - 1)
+
+
+def indexEqualsValueHelper(array, leftIdx, rightIdx):
+    if leftIdx > rightIdx:
+        return -1
+
+    midIdx = leftIdx + (rightIdx - leftIdx) // 2
+    midValue = array[midIdx]
+
+    if midValue < midIdx:
+        return indexEqualsValueHelper(array, midIdx + 1, rightIdx)
+    elif midValue == midIdx and midIdx == 0:
+        return midIdx
+    elif midValue == midIdx and array[midIdx - 1] < midIdx - 1:
+        return midIdx
+    else:
+        return indexEqualsValueHelper(array, leftIdx, midIdx - 1)
