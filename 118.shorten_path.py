2f4c66fe97b4ed1548cc43e2c7b7ad4eedadf74a
bigname <tuanhiep.tran88@gmail.com>
2021-06-02 08:10:05 -0600

    Shorten Path Solution

---
+++ 118.shorten_path.py
@@ -0,0 +1,22 @@
+def shortenPath(path):
+    # O(n) time | O(n) space - where n is the length of the path
+    startsWithSlash = path[0] == "/"
+    tokens = filter(isImportantToken, path.split("/"))
+    stack = []
+    if startsWithSlash:
+        stack.append("")
+    for token in tokens:
+        if token == "..":
+            if len(stack) == 0 or stack[-1] == "..":
+                stack.append(token)
+            elif stack[-1] != "":
+                stack.pop()
+        else:
+            stack.append(token)
+    if len(stack) == 1 and stack[0] == "":
+        return "/"
+    return "/".join(stack)
+
+
+def isImportantToken(token):
+    return len(token) > 0 and token != "."
